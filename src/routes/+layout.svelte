1c126725acca075a6e13b352437d121b380c669f
Prince Gabriel Caba <prince.caba27@gmail.com>
2024-02-19 15:44:47 +0800

    login form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.css";
+  import { afterNavigate } from "$app/navigation";
+
+  afterNavigate(() => {
+    // @ts-ignore
+    window.HSStaticMethods.autoInit();
+  });
 </script>
 
 <slot />
