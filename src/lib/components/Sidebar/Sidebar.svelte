5692fe2052a3bbf1573c558983ef358790f6fd68
Marbert <zedchixx23@gmail.com>
2024-02-20 14:23:29 +0800

    Create sidebar component and sub components

---
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -0,0 +1,80 @@
+<script>
+  import {
+    Briefcase,
+    LayoutDashboard,
+    PersonStanding,
+    ShieldCheck,
+    AppWindow,
+    Shapes,
+    Flag,
+    Sticker,
+  } from "lucide-svelte";
+  import SidebarUrl from "./subcomponents/Sidebar-Url.svelte";
+    import SidebarDivider from "./subcomponents/Sidebar-Divider.svelte";
+</script>
+
+<div
+  id="application-sidebar"
+  class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700"
+>
+  <div class="px-6">
+    <a
+      class="flex-none text-xl font-semibold dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+      href="#"
+      aria-label="Brand">POSO ADMIN</a
+    >
+  </div>
+  <nav
+    class="hs-accordion-group p-6 w-full flex flex-col flex-wrap"
+    data-hs-accordion-always-open
+  >
+    <ul class="space-y-1.5">
+      <SidebarDivider title="Analytics" />
+      <li>
+        <SidebarUrl name="Dashboard" isActive={true} route="#">
+          <LayoutDashboard />
+        </SidebarUrl>
+      </li>
+      <SidebarDivider title="Tickets" />
+      <li class="hs-accordion" id="users-accordion">
+        <SidebarUrl name="Payment" route="#">
+          <Briefcase />
+        </SidebarUrl>
+      </li>
+      <li class="hs-accordion" id="account-accordion">
+        <SidebarUrl name="Tickets" route="#">
+          <Sticker />
+        </SidebarUrl>
+      </li>
+      <SidebarDivider title="Employees" />
+      <li class="hs-accordion" id="projects-accordion">
+        <SidebarUrl name="Enforcer" route="#">
+          <PersonStanding />
+        </SidebarUrl>
+      </li>
+      <li>  
+        <SidebarUrl name="Admin" route="#">
+          <ShieldCheck />
+        </SidebarUrl> 
+      </li>
+      <SidebarDivider title="System"/>
+      <li>
+        <SidebarUrl name="Violations" route="#">
+          <AppWindow />
+        </SidebarUrl>
+
+      </li>
+      <li>
+        <SidebarUrl name="Vehicle Types" route="#">
+          <Shapes />
+        </SidebarUrl>
+      </li>
+      <li>
+        <SidebarUrl name="Traffic Post" route="#">
+          <Flag />
+        </SidebarUrl>
+      </li>
+    </ul>
+  </nav>
+</div>
+<!-- End Sidebar -->
---
+++ src/lib/components/Sidebar/subcomponents/Sidebar-Divider.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let title: String = "";
+
+</script>
+
+<li class="p-2">{title}</li>
