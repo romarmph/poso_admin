e1ec7f3c91d0df4efd184e0a82442c5d3a92ed94
Marbert <zedchixx23@gmail.com>
2024-02-26 09:21:55 +0800

    tickets commit

---
+++ src/lib/components/tickets/input/input.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let headtitle: String = "";
+    export let placeholder: string="";
+    export let id: string="";
+    export let type: string="";
+    export let py: number;
+    export let px: number;
+    export let pe: number;
+    export let w: number;
+  </script>
+<div class="sm:col-span-9">
+    <li class="p-3" style="font-weight: bold;">{headtitle}</li>
+    <input
+        id={id}
+        type={type}
+        class="py-{py} px-{px} pe-{pe} block w-{w} border-black-200 shadow-sm text-sm rounded-lg focus:border-blue-500"
+        placeholder={placeholder}
+    />
+
+</div>
--- src/routes/(app)/tickets/+page.svelte
+++ src/routes/(app)/tickets/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Tickets</h1>
+<script>
+    import Tables from "$lib/components/tickets/tables/tables.svelte";
+    import Smallbox from "$lib/components/tickets/input/Smallbox.svelte";
+    import Button from "$lib/components/tickets/Button/Button.svelte";
+</script>
+
+<h1 style="font-weight: bold;">Tickets</h1>
+<header style="display: flex; justify-content: flex-end; padding: 10px;">
+    <div style="display: flex; align-items: center;">
+        <Smallbox id="search" type="text" py={3} px={3} pe={11} w={30} mr={4} placeholder="Search"/>    
+        <Button buttonname="Add Tickets" marginright={10} bgcolor="blue" color="white"/>
+    </div>
+    
+
+  </header>
+<Tables/>
