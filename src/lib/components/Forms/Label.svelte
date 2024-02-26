18db8e33eeb281c14348e0dca6ac96f171f21a33
Marbert <zedchixx23@gmail.com>
2024-02-26 14:29:53 +0800

    commit Ticket

--- src/lib/components/Forms/Label.svelte
+++ src/lib/components/Forms/Label.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   export let id: string;
+  export let classNames: string="";
 </script>
 
-<label for={id} class="block text-sm font-medium mb-2 dark:text-white">
+<label for={id} class="block text-sm font-medium mb-2 dark:text-white {classNames}">
   <slot />
 </label>
+
