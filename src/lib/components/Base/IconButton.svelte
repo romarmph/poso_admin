05a4a2e027259b623cfa564255957e3d3856db1e
Romar Macaraeg <roamrcm@gmail.com>
2024-02-27 15:30:22 +0800

    Refactor IconButton and ListTile components

--- src/lib/components/Base/IconButton.svelte
+++ src/lib/components/Base/IconButton.svelte
@@ -1,45 +1,44 @@
 <script lang="ts">
-    export let style: "solid" | "outline" | "ghost" | "soft" | "white" =
-        "solid";
-    export let size: "small" | "default" | "large" = "default";
-    export let color: "blue" | "teal" | "green" | "red" | "yellow" | "gray" =
-        "blue";
+  export let style: "solid" | "outline" | "ghost" | "soft" | "white" = "solid";
+  export let size: "small" | "default" | "large" = "default";
+  export let color: "blue" | "teal" | "green" | "red" | "yellow" | "gray" =
+    "blue";
 
-    let buttonSize = {
-        small: "size-[38px]",
-        default: "py-3 px-4",
-        large: "py-4 px-5",
-    };
+  let buttonSize = {
+    small: "size-[38px]",
+    default: "size-[46px]",
+    large: "size-[62px]",
+  };
 
-    const base = `${buttonSize[size]} flex flex-shrink-0 justify-center items-center gap-2 text-sm font-semibold rounded-lg border border-transparent`;
+  const base = `${buttonSize[size]} flex flex-shrink-0 justify-center items-center gap-2 text-sm font-semibold rounded-lg border border-transparent`;
 </script>
 
 {#if style === "solid"}
-    <button class="{base} bg-{color}-600 text-white hover:bg-{color}-700">
-        <slot />
-    </button>
+  <button class="{base} bg-{color}-600 text-white hover:bg-{color}-700">
+    <slot />
+  </button>
 {:else if style === "outline"}
-    <button
-        class="{base} border-gray-200 text-gray-500 hover:border-{color}-600 hover:text-{color}-600 dark:border-gray-700 dark:text-gray-400 dark:hover:text-{color}-500 dark:hover:border-{color}-600"
-    >
-        <slot />
-    </button>
+  <button
+    class="{base} border-gray-200 text-gray-500 hover:border-{color}-600 hover:text-{color}-600 dark:border-gray-700 dark:text-gray-400 dark:hover:text-{color}-500 dark:hover:border-{color}-600"
+  >
+    <slot />
+  </button>
 {:else if style === "ghost"}
-    <button
-        class="{base} text-{color}-600 hover:bg-{color}-100 hover:text-{color}-800 dark:text-{color}-500 dark:hover:bg-{color}-800/30 dark:hover:text-{color}-400"
-    >
-        <slot />
-    </button>
+  <button
+    class="{base} text-{color}-600 hover:bg-{color}-100 hover:text-{color}-800 dark:text-{color}-500 dark:hover:bg-{color}-800/30 dark:hover:text-{color}-400"
+  >
+    <slot />
+  </button>
 {:else if style === "soft"}
-    <button
-        class="{base} bg-{color}-100 text-{color}-800 hover:bg-{color}-200 dark:hover:bg-{color}-900 dark:text-{color}-400"
-    >
-        <slot />
-    </button>
+  <button
+    class="{base} bg-{color}-100 text-{color}-800 hover:bg-{color}-200 dark:hover:bg-{color}-900 dark:text-{color}-400"
+  >
+    <slot />
+  </button>
 {:else if style === "white"}
-    <button
-        class="{base} border-gray-200 bg-white text-{color}-800 shadow-sm hover:bg-gray-50 dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800"
-    >
-        <slot />
-    </button>
+  <button
+    class="{base} border-gray-200 bg-white text-{color}-800 shadow-sm hover:bg-gray-50 dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800"
+  >
+    <slot />
+  </button>
 {/if}
--- src/lib/components/Base/ListTile.svelte
+++ src/lib/components/Base/ListTile.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-    import { ArrowLeft, Plus } from "lucide-svelte";
+  import { Plus } from "lucide-svelte";
 
-    import {
-        Button,
-        Divider,
-        TextInput,
-        Label,
-        Grid,
-        GridCol,
-        IconButton,
-    } from "$lib/Components";
-    export let name: string = "";
-    export let amount: string = "";
+  import { Grid, GridCol, IconButton } from "$lib/Components";
+  export let name: string = "";
+  export let amount: string = "";
 </script>
 
-<div class="p-4">
-    <div class="p-3 my-3">
-        <Grid columns="grid-cols-6" gap="gap-4">
-            <GridCol colSpan="col-span-3">{name}</GridCol>
-            <GridCol colSpan="col-span-2">{amount}</GridCol>
-            <GridCol
-                colSpan="col-span-1"
-                className="flex justify-end items-center"
-            >
-                <IconButton><Plus /></IconButton>
-            </GridCol>
-        </Grid>
-    </div>
+<div class="px-4 my-1">
+  <div class="p-2 rounded-md bg-gray-50 shadow-sm hover:bg-gray-100">
+    <Grid columns="grid-cols-6" gap="gap-4">
+      <GridCol
+        colSpan="col-span-4"
+        className="text-gray-600 text-md font-semibold flex items-center"
+        >{name}</GridCol
+      >
+      <GridCol
+        colSpan="col-span-1"
+        className="text-md text-red-400 justify-center font-bold flex items-center"
+        >{amount}</GridCol
+      >
+      <GridCol colSpan="col-span-1" className="flex justify-end items-center">
+        <IconButton style="ghost" size="small"><Plus /></IconButton>
+      </GridCol>
+    </Grid>
+  </div>
 </div>
