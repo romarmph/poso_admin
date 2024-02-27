7a09af9eb367f3f8716aaccfa14163dff5b1885f
PrinceCaba <prince.caba27@gmail.com>
2024-02-27 16:20:05 +0800

    update add delete btn

--- src/routes/(app)/enforcer/+page.svelte
+++ src/routes/(app)/enforcer/+page.svelte
@@ -3,6 +3,13 @@
     import SidebarDivider from "$lib/components/Sidebar/subcomponents/Sidebar-Divider.svelte";
     import TextInput from "$lib/components/Forms/TextInput.svelte";
     import Label from "$lib/components/Forms/Label.svelte";
+    import {
+        Modal,
+        ModalBody,
+        ModalFooter,
+        ModalHeader,
+    } from "$lib/components/Modal/Modal";
+    import { Button } from "$lib/Components";
 </script>
 
 <header style="display: flex; align-items: center;">
@@ -19,11 +26,12 @@
     <button
         type="button"
         class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        data-hs-overlay="#hs-focus-management-modal"
+        data-hs-overlay="#hs-add_enforcer-modal"
     >
         Add Enforcer
     </button>
 </div>
+<!--Table -->
 
 <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <!-- Card -->
@@ -159,21 +167,24 @@
                                 </td>
 
                                 <td class="size-px whitespace-nowrap">
-                                    <button
-                                        style="background-color: green;"
-                                        type="button"
-                                        class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                        data-hs-overlay="#hs-edit-modal"
+                                    <div
+                                        class="inline-flex rounded-lg shadow-sm"
                                     >
-                                        Edit
-                                    </button>
-                                    <button
-                                        type="button"
-                                        class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                        data-hs-overlay="#hs-del-modal"
-                                    >
-                                        Delete
-                                    </button>
+                                        <button
+                                            type="button"
+                                            class="py-2 px-3 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                            data-hs-overlay="#hs-edit_enforcer-modal"
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            class="py-2 px-3 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                            data-hs-overlay="#hs-del_enforcer-modal"
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
                                 </td>
                             </tr>
                         </tbody>
@@ -185,248 +196,100 @@
 </div>
 
 <!-- Add modal -->
-<div
-    id="hs-focus-management-modal"
-    class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
->
-    <div
-        class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto"
-    >
-        <div
-            class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-        >
-            <div
-                class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700"
-            >
-                <h3 class="font-bold text-gray-800 dark:text-white">
-                    Add Enforcer
-                </h3>
-                <button
-                    type="button"
-                    class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                    data-hs-overlay="#hs-focus-management-modal"
-                >
-                    <span class="sr-only">Close</span>
-                    <svg
-                        class="flex-shrink-0 size-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        ><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
-                    >
-                </button>
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">First Name</Label>
+<Modal modalId="hs-add_enforcer-modal">
+    <ModalHeader slot="header">Add Enforcer</ModalHeader>
 
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer first name here"
-                />
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">Middle Name</Label>
+    <ModalBody slot="body">
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">First Name</Label>
 
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer middle name here"
-                />
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">Last Name</Label>
-
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer Last name here"
-                />
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">Employee No.</Label>
-
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer Employee number here"
-                />
-            </div>
-            <div
-                class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-700"
-            >
-                <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                    data-hs-overlay="#hs-focus-management-modal"
-                >
-                    Cancel
-                </button>
-                <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                >
-                    Save
-                </button>
-            </div>
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer first name here"
+            />
         </div>
-    </div>
-</div>
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">Middle Name</Label>
+
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer middle name here"
+            />
+        </div>
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">Last Name</Label>
+
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer Last name here"
+            />
+        </div>
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">Employee No.</Label>
+
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer Employee number here"
+            />
+        </div>
+    </ModalBody>
+
+    <ModalFooter slot="footer"></ModalFooter>
+</Modal>
 
 <!-- Update modal -->
-<div
-    id="hs-edit-modal"
-    class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
->
-    <div
-        class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto"
-    >
-        <div
-            class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-        >
-            <div
-                class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700"
-            >
-                <h3 class="font-bold text-gray-800 dark:text-white">
-                    Update Enforcer
-                </h3>
-                <button
-                    type="button"
-                    class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                    data-hs-overlay="#hs-edit-modal"
-                >
-                    <span class="sr-only">Close</span>
-                    <svg
-                        class="flex-shrink-0 size-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        ><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
-                    >
-                </button>
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">First Name</Label>
+<Modal modalId="hs-edit_enforcer-modal">
+    <ModalHeader slot="header">Update Enforcer</ModalHeader>
 
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer first name here"
-                />
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">Middle Name</Label>
+    <ModalBody slot="body">
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">First Name</Label>
 
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer middle name here"
-                />
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">Last Name</Label>
-
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer Last name here"
-                />
-            </div>
-            <div class="p-4 overflow-y-auto">
-                <Label id="input-label">Employee No.</Label>
-
-                <TextInput
-                    id="input-Label"
-                    placeholder="Type enforcer Employee number here"
-                />
-            </div>
-            <div
-                class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-700"
-            >
-                <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                    data-hs-overlay="#hs-edit-modal"
-                >
-                    Cancel
-                </button>
-                <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                >
-                    Save
-                </button>
-            </div>
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer first name here"
+            />
         </div>
-    </div>
-</div>
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">Middle Name</Label>
+
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer middle name here"
+            />
+        </div>
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">Last Name</Label>
+
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer Last name here"
+            />
+        </div>
+        <div class="p-4 overflow-y-auto">
+            <Label id="input-label">Employee No.</Label>
+
+            <TextInput
+                id="input-Label"
+                placeholder="Type enforcer Employee number here"
+            />
+        </div>
+    </ModalBody>
+
+    <ModalFooter slot="footer"></ModalFooter>
+</Modal>
 
 <!-- delete modal -->
-<div
-    id="hs-del-modal"
-    class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
->
-    <div
-        class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto"
-    >
-        <div
-            class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-        >
-            <div
-                class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700"
-            >
-                <button
-                    type="button"
-                    class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                    data-hs-overlay="#hs-del-modal"
-                >
-                    <span class="sr-only">Close</span>
-                    <svg
-                        class="flex-shrink-0 size-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        ><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
-                    >
-                </button>
-            </div>
-            <div style="font-size: 35px; text-align:center">Are you sure</div>
-            <div style="font-size: 20px; text-align:center">
-                Delete this user
-            </div>
-            <div
-                class="justify-center text-center py-3 px-4 border-b dark:border-gray-700"
-            >
-                <div class="flex justify-between">
-                    <button
-                        type="button"
-                        class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                        data-hs-overlay="#hs-cancel-modal"
-                        style="width: 100%; height: 40px; display: flex; align-items: center; justify-content: center; margin: auto; margin-right: 10px"
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        type="button"
-                        class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-red-500 text-white hover:bg-red-600 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                        data-hs-overlay="#hs-Delete-modal"
-                        style="width: 100%; height: 40px; display: flex; align-items: center; justify-content: center; margin: auto;"
-                    >
-                        Delete
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<Modal modalId="hs-del_enforcer-modal">
+    <ModalHeader slot="header"></ModalHeader>
+
+    <ModalBody slot="body">
+        <div style="font-size: 35px; text-align:center">Are you sure</div>
+        <div style="font-size: 20px; text-align:center">Delete this user</div>
+    </ModalBody>
+
+    <ModalFooter slot="footer">
+        <Button style="soft" color="gray">Cancel</Button>
+        <Button color="red">Delete</Button>
+    </ModalFooter>
+</Modal>
--- src/routes/(app)/tickets/add/+page.svelte
+++ src/routes/(app)/tickets/add/+page.svelte
@@ -4,6 +4,13 @@
   import Tables from "$lib/components/Table/tables.svelte";
 
   import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+  } from "$lib/components/Modal/Modal";
+
+  import {
     Button,
     Divider,
     TextInput,
@@ -19,86 +26,28 @@
   <button><ArrowLeft style="margin-right: 10px;" /></button>
   <h1 style="font-weight: bold;">New Ticket</h1>
 </header>
+
+<!-- Add Violations-->
 <div class="flex justify-end">
-  <button
-    type="button"
-    class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-    data-hs-overlay="#hs-vertically-centered-modal"
-  >
-    Add Violatons
-  </button>
+  <Button modalId="hs-vertically-centered-modal">Add Violations</Button>
 </div>
 
-<!--Modal-->
+<Modal modalId="hs-vertically-centered-modal">
+  <ModalHeader slot="header">Add Violations</ModalHeader>
 
-<div
-  id="hs-vertically-centered-modal"
-  class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
->
-  <div
-    class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto"
-  >
-    <div
-      class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-    >
-      <div
-        class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700"
-      >
-        <h3 class="font-bold text-gray-800 dark:text-white">Add Violations</h3>
-        <button
-          type="button"
-          class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          data-hs-overlay="#hs-vertically-centered-modal"
-        >
-          <span class="sr-only">Close</span>
-          <svg
-            class="flex-shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
-          >
-        </button>
-      </div>
-      <div class="p-4 overflow-y-auto">
-        <input
-          type="email"
-          id="input-label"
-          class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-          placeholder="Search"
-        />
-      </div>
-      <ListTile name="Illegal Parking" amount="500" />
-      <ListTile name="Illegal Parking" amount="500" />
-      <ListTile name="No Driver's License" amount="500" />
+  <ModalBody slot="body">
+    <TextInput id="search" placeholder="Add Violation" />
 
-      <div
-        class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-700"
-      >
-        <button
-          type="button"
-          class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          data-hs-overlay="#hs-vertically-centered-modal"
-        >
-          Close
-        </button>
-        <button
-          type="button"
-          class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          data-hs-overlay="#hs-vertically-centered-modal"
-        >
-          Save
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
+    <ListTile name="Violation 1" amount="500" />
+    <ListTile name="Violation 1" amount="500" />
+    <ListTile name="Violation 1" amount="500" />
+    <ListTile name="Violation 1" amount="500" />
+    <ListTile name="Violation 1" amount="500" />
+    <ListTile name="Violation 1" amount="500" />
+  </ModalBody>
+
+  <ModalFooter slot="footer"></ModalFooter>
+</Modal>
 
 <Grid columns="grid-cols-6" gap="gap-4">
   <!-- Violator Details -->
