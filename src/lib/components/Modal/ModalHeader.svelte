0a96c3122e120e97f8404f5a64094623d8ef2149
Romar Macaraeg <roamrcm@gmail.com>
2024-02-27 14:55:30 +0800

    Add modal functionality to Button component and create a sample modal in +page.svelte

---
+++ src/lib/components/Modal/ModalHeader.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+
+  const modalId = getContext("modalId");
+</script>
+
+<div
+  class="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700"
+>
+  <h3 class="font-bold text-gray-800 dark:text-white">
+    <slot />
+  </h3>
+  <button
+    type="button"
+    class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+    data-hs-overlay="#{modalId}"
+  >
+    <span class="sr-only">Close</span>
+    <svg
+      class="flex-shrink-0 size-4"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      ><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
+    >
+  </button>
+</div>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
+  import {
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+  } from "$lib/components/Modal/Modal";
+
+  import { Button } from "$lib/Components";
 </script>
 
 <h1 class="text-2xl font-bold text-gray-700">Tickets</h1>
+
+<!-- SAMPLE -->
+<Button modalId="test-modal">Open</Button>
+
+<Modal modalId="test-modal">
+  <ModalHeader slot="header">My Modal</ModalHeader>
+
+  <ModalBody slot="body"></ModalBody>
+
+  <ModalFooter slot="footer"></ModalFooter>
+</Modal>
