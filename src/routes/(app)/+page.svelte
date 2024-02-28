b77cd6a7c9601a517a5d6769b93c4b7282b2df93
Romar Macaraeg <roamrcm@gmail.com>
2024-02-28 09:40:21 +0800

    Refactor Modal component and remove unused code

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -12,19 +12,3 @@
 </script>
 
 <h1 class="text-2xl font-bold text-gray-700">Tickets</h1>
-
-<!-- SAMPLE -->
-<Button modalId="test-modal">Open</Button>
-
-<Modal modalId="test-modal">
-  <ModalHeader slot="header">My Modal</ModalHeader>
-
-  <ModalBody slot="body"></ModalBody>
-
-  <ModalFooter slot="footer"></ModalFooter>
-</Modal>
-
-<TicketStatus status="paid" />
-<TicketStatus status="unpaid" />
-<TicketStatus status="overdue" />
-<TicketStatus status="transferred" />
