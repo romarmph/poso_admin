67829329c4e82e81df1723d81754564d2b2d719e
shit <zedchixx23@gmail.com>
2024-03-04 14:36:36 +0800

    payments

---
+++ src/lib/components/Base/Card.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+export let name: string="";
+// export let iconCard: string="<Users/>";
+import {TrendingDown, TrendingUp, Users } from "lucide-svelte";
+
+
+</script>
+
+
+<div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-800">
+    <div class="p-4 md:p-5 flex gap-x-4">
+      <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
+ <Users/>
+    </div>
+
+      <div class="grow">
+        <div class="flex items-center gap-x-2">
+          <p class="text-xs uppercase tracking-wide text-gray-500">
+            {name}
+          </p>
+        </div>
+        <div class="mt-1 flex items-center gap-x-2">
+          <h3 class="text-xl sm:text-2xl font-medium text-gray-800 dark:text-gray-200">
+            72,540
+          </h3>
+          <span class="inline-flex items-center gap-x-1 py-0.5 px-2 rounded-full bg-green-100 text-green-900 dark:bg-green-800 dark:text-green-100">
+            <slot/>
+            <span class="inline-block text-xs font-medium">
+              12.5%
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ src/routes/(app)/payment/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+import Card from "$lib/components/Base/Card.svelte";
+import {TrendingDown, TrendingUp, Users } from "lucide-svelte";
+
+</script>
+
+
+<header style="display: flex; align-items: center;">
+    <h1 style="font-weight: bold;">Payment</h1>
+   
+  </header>
+
+
+<!-- Card Section -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <!-- Grid -->
+    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
+      <!-- Card -->
+     <Card name="Total Tickets">
+        <TrendingDown/>
+     </Card> 
+      <!-- End Card -->
+  
+      <!-- Card -->
+      <Card name="Total Tickets">
+        <TrendingUp/>
+     </Card> 
+
+      <!-- End Card -->
+  
+      <!-- Card -->
+      <Card name="Total Tickets">
+        <TrendingDown/>
+     </Card> 
+
+      <!-- End Card -->
+  
+      <!-- Card -->
+      <Card name="Total Tickets">
+        <TrendingDown/>
+     </Card> 
+
+      <!-- End Card -->
+    </div>
+    <!-- End Grid -->
+  </div>
+  <!-- End Card Section -->
