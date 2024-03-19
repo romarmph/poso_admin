c10eac69a973a7a07576b6f6c827d4c54420c221
Romar Macaraeg <roamrcm@gmail.com>
2024-03-19 11:21:36 +0800

    Add param matching for ticket id

--- src/routes/(app)/payment/+page.svelte
+++ src/routes/(app)/payment/+page.svelte
@@ -1,9 +1,13 @@
 <script>
-
-import Card from "$lib/components/Base/Card.svelte";
-import {TrendingDown, TrendingUp, Users, Ticket, TicketCheck, TicketX } from "lucide-svelte";
-
-
+  import Card from "$lib/components/Base/Card.svelte";
+  import {
+    TrendingDown,
+    TrendingUp,
+    Users,
+    Ticket,
+    TicketCheck,
+    TicketX,
+  } from "lucide-svelte";
 </script>
 
 <header style="display: flex; align-items: center;">
@@ -12,38 +16,36 @@ import {TrendingDown, TrendingUp, Users, Ticket, TicketCheck, TicketX } from "lu
 
 <!-- Card Section -->
 <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-    <!-- Grid -->
-    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
-      <!-- Card -->
-     <Card name="Total Tickets">
-        <TrendingDown/>
-     </Card> 
-      <!-- End Card -->
-  
-      <!-- Card -->
-      <Card name="Total Tickets">
-        <TrendingDown/>
-     </Card> 
+  <!-- Grid -->
+  <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
+    <!-- Card -->
+    <Card name="Total Tickets">
+      <TrendingDown />
+    </Card>
+    <!-- End Card -->
 
-      <!-- End Card -->
-  
-      <!-- Card -->
-      <Card name="Total Tickets">
-        <TrendingDown/>
-     </Card> 
+    <!-- Card -->
+    <Card name="Total Tickets">
+      <TrendingDown />
+    </Card>
 
-      <!-- End Card -->
-  
-      <!-- Card -->
-      <Card name="Total Tickets">
-        <TrendingDown/>
-     </Card> 
+    <!-- End Card -->
 
-      <!-- End Card -->
-    </div>
-    <!-- End Grid -->
+    <!-- Card -->
+    <Card name="Total Tickets">
+      <TrendingDown />
+    </Card>
 
+    <!-- End Card -->
+
+    <!-- Card -->
+    <Card name="Total Tickets">
+      <TrendingDown />
+    </Card>
+
+    <!-- End Card -->
   </div>
   <!-- End Grid -->
 </div>
+<!-- End Grid -->
 <!-- End Card Section -->
