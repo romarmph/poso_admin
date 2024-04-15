c18d6e2de1230bed2d0e0fbfe3fc17d7642755f5
shit <zedchixx23@gmail.com>
2024-04-15 16:04:06 +0800

    commit

---
+++ src/lib/components/Base/VehicleSize.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let big_vehicle: boolean;
+</script>
+
+{#if big_vehicle}
+    <span class="bg-teal-500 text-white text-xs px-2 py-1 rounded-lg">Big</span>
+{:else}
+    <span class="bg-blue-500 text-white text-xs px-2 py-1 rounded-lg"
+        >Small</span
+    >
+{/if}
--- src/routes/(app)/traffic-post/+page.svelte
+++ src/routes/(app)/traffic-post/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import {
         Modal,
         ModalBody,
@@ -6,11 +6,78 @@
         ModalHeader,
     } from "$lib/components/Overlays/Modal/Modal";
 
-    import { Button, TextInput,Label,Tables,TableBody,TableHead,Dropdown } from "$lib/Components";
-    import {
-        MoreHorizontal,
-        Download,
-    } from "lucide-svelte";
+    import { Button, TextInput, Label } from "$lib/Components";
+    import DataList from "$lib/components/Supabase/DataList.svelte";
+    import { flexRender, type ColumnDef } from "@tanstack/svelte-table";
+    import TanTable from "$lib/components/Table/TanTable.svelte";
+    import EnabledStatus from "$lib/components/Base/EnabledStatus.svelte";
+    import ViolationFines from "$lib/components/Customs/ViolationFines.svelte";
+    import RowActions from "$lib/components/Table/Partials/RowActions.svelte";
+
+    const columns: ColumnDef<Types.TrafficPost>[] = [
+        {
+            accessorKey: "name",
+            cell: (info) => info.getValue(),
+            footer: (info) => info.column.id,
+            header: "Traffic Post",
+        },
+        {
+            accessorKey: "address",
+            cell: (info) => info.getValue(),
+            footer: (info) => info.column.id,
+            header: "Address",
+        },
+
+        {
+            accessorKey: "created_at",
+            cell: (info) => info.getValue(),
+            footer: (info) => info.column.id,
+            header: "Created At",
+            accessorFn: (row) => {
+                console.log(row);
+                return new Date(row.created_at).toDateString();
+            },
+        },
+        {
+            accessorKey: "created_by",
+            cell: (info) => info.getValue(),
+            footer: (info) => info.column.id,
+            header: "Created By",
+        },
+        {
+            accessorKey: "updated_at",
+            cell: (info) => info.getValue(),
+            footer: (info) => info.column.id,
+            header: "Updated At",
+            accessorFn: (row) => new Date(row.updated_at).toDateString(),
+        },
+        {
+            accessorKey: "updated_by",
+            cell: (info) => info.getValue(),
+            footer: (info) => info.column.id,
+            header: "Updated By",
+        },
+        {
+            accessorKey: "id",
+            cell: (info) =>
+                flexRender(RowActions, {
+                    id: info.getValue(),
+                    fireEdit: () => {
+                        console.log("Edit");
+                    },
+                    fireView: () => {
+                        console.log("View");
+                    },
+                    fireDelete: () => {
+                        console.log("Delete");
+                    },
+                }),
+            header: "Actions",
+            enableSorting: false,
+        },
+    ];
+
+    export let data;
 </script>
 
 <header style="display: flex; align-items: center;">
@@ -21,80 +88,9 @@
 </header>
 
 <!--Table -->
-<Tables>
-    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-        <thead class="bg-gray-50 dark:bg-slate-800">
-            <tr>
-                <TableHead name="" classes="ps-6 py-3 text-start"
-                    ><input
-                        type="checkbox"
-                        class="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                        id="hs-at-with-checkboxes-main"
-                    />
-                </TableHead>
-                <TableHead name="Post Name" classes="px-6 py-3 text-start" />
-                <TableHead name="Location" classes="px-6 py-3 text-start" />
-                <TableHead name="Status" classes="px-6 py-3 text-start" />
-                <TableHead name="Created By:" classes="px-6 py-3 text-start" />
-                <TableHead name="Created At:" classes="px-6 py-3 text-start" />
-            </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-            <tr>
-                <TableBody classed="ps-6 py-3 text-start">
-                    <input
-                        type="checkbox"
-                        class="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                        id="hs-at-with-checkboxes-1"
-                    />
-                </TableBody>
-                <TableBody>123</TableBody>
-                <TableBody>123</TableBody>
-                <TableBody>
-                    <button
-                        type="button"
-                        class="block"
-                        data-hs-overlay="#hs-ai-invoice-modal"
-                    >
-                        <span
-                            class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500"
-                        >
-                            Enable
-                        </span>
-                    </button></TableBody
-                >
-                <TableBody>123</TableBody>
-                <TableBody>123</TableBody>
-                <TableBody
-                    ><div
-                        class="group inline-flex items-center divide-x divide-gray-300 border border-gray-300 bg-white shadow-sm rounded-lg transition-all dark:divide-gray-700 dark:bg-slate-700 dark:border-gray-700"
-                    >
-                        <div class="hs-tooltip inline-block">
-                            <a
-                                class="hs-tooltip-toggle py-1.5 px-2 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-s-md bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                href="#"
-                            >
-                                <Download />
-                                <span
-                                    class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm dark:bg-slate-700"
-                                    role="tooltip"
-                                >
-                                    Download PDF
-                                </span>
-                            </a>
-                        </div>
-                        <Dropdown
-                            over="hs-edit-TPost-modal"
-                            del="hs-del-TPost-modal"
-                            ><MoreHorizontal /></Dropdown
-                        >
-                    </div></TableBody
-                >
-            </tr>
-        </tbody>
-    </table>
-</Tables>
-
+<DataList table="traffic_posts" let:data initData={data.trafficPost ?? []}>
+    <TanTable {data} {columns}></TanTable>
+</DataList>
 <!-- Add Traffic Post modal -->
 <Modal modalId="hs-add-TPost-modal">
     <ModalHeader>Add Traffic Post</ModalHeader>
