e534a981306db44bdd941b5309680773237a357a
Romar Macaraeg <roamrcm@gmail.com>
2024-04-23 08:49:06 +0800

    Update header to show current user name on dropdown

--- src/lib/components/Header/AvatarDropdown.svelte
+++ src/lib/components/Header/AvatarDropdown.svelte
@@ -1,14 +1,21 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  import { CircleUserRound } from "lucide-svelte";
+  const user: Types.Employees = getContext("user");
+</script>
+
 <div class="hs-dropdown relative inline-flex [--placement:bottom-right]">
   <button
     id="hs-dropdown-with-header"
     type="button"
     class="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
   >
-    <img
-      class="inline-block size-[38px] rounded-full ring-2 ring-white dark:ring-gray-800"
-      src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-      alt=""
-    />
+    <!-- <img -->
+    <!--   class="inline-block size-[38px] rounded-full ring-2 ring-white dark:ring-gray-800" -->
+    <!--   src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" -->
+    <!--   alt="" -->
+    <!-- /> -->
+    <CircleUserRound size={32} strokeWidth={1} />
   </button>
 
   <div
@@ -18,7 +25,8 @@
     <div class="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg dark:bg-gray-700">
       <p class="text-sm text-gray-500 dark:text-gray-400">Signed in as</p>
       <p class="text-sm font-medium text-gray-800 dark:text-gray-300">
-        james@site.com
+        {user.first_name}
+        {user.last_name}
       </p>
     </div>
     <div class="mt-2 py-2 first:pt-0 last:pb-0">
