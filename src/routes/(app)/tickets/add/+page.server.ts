87ddc7c47e124232ac6a00ce02e022616909a33e
Romar Macaraeg <roamrcm@gmail.com>
2024-04-24 01:27:10 +0800

    Update ticket add form

---
+++ src/routes/(app)/tickets/add/+page.server.ts
@@ -0,0 +1,27 @@
+import type { Actions } from "@sveltejs/kit";
+import type { PageServerLoad } from "../$types";
+import { zod } from "sveltekit-superforms/adapters";
+import { superValidate } from "sveltekit-superforms";
+import { ticketSchema } from "$lib/schemas/app";
+
+export const load: PageServerLoad = async ({ locals: { supabase } }) => {
+  const ticketForm = await superValidate(zod(ticketSchema));
+  const { data: violations } = await supabase.from("violations").select().is("deleted_by", null);
+  const { data: vehicleTypes } = await supabase.from("vehicle_types").select().is("deleted_by", null);
+  return {
+    ticketForm,
+    violations,
+    vehicleTypes,
+  };
+}
+
+export const actions: Actions = {
+  create: async ({ request, locals: { supabase } }) => {
+    const ticketForm = await superValidate(request, zod(ticketSchema));
+
+    console.log(ticketForm);
+    return {
+      ticketForm,
+    }
+  }
+}
--- src/routes/(app)/tickets/add/+page.svelte
+++ src/routes/(app)/tickets/add/+page.svelte
@@ -1,88 +1,143 @@
 <script>
-  import { ArrowLeft } from "lucide-svelte";
-  import {
-    Button,
-    Divider,
-    TextInput,
-    Label,
-    Grid,
-    GridCol,
-    DatePicker,
-  } from "$lib/Components";
+  import { Button, Divider, TextInput } from "$lib/Components";
+  import Grid from "$lib/components/Layout/Grid.svelte";
+  import GridCol from "$lib/components/Layout/GridCol.svelte";
+  import Overlay from "$lib/components/Overlays/Overlay.svelte";
+  import { overlayStore } from "$lib/stores/overlayStore";
+  const { open, close } = overlayStore;
+
+  export let data;
+  let selectedViolations = [];
 </script>
 
 <svelte:head><title>Add Ticket</title></svelte:head>
 
 <header style="display: flex; align-items: center;">
-  <button><ArrowLeft style="margin-right: 10px;" /></button>
-  <h1 style="font-weight: bold;">New Ticket</h1>
-  <div style="margin-left:auto">
+  <h1 class="text-2xl font-bold text-gray-800">New Ticket</h1>
+  <div class="ms-auto">
     <Button data-hs-overlay="#hs-add-violation-modal">Add Violations</Button>
   </div>
 </header>
-<form action="">
-  <Grid columns="grid-cols-7" gap="gap-4">
+<form action="?/create" method="POST" class="mt-4">
+  <Divider strokeWidth={1}>
+    <h2 slot="left" class="text-gray-600 text-xl">Violator Information</h2>
+  </Divider>
+  <Grid columns="grid-cols-6" gap="gap-4" classNames="my-4">
     <GridCol colSpan="col-span-2">
-      <Label id="" classNames="mt-3">Violator</Label>
-      <Divider border={2} />
-      <div class="my-4">
-        <Label id="last_name">Last Name</Label>
-        <TextInput type="text" id="last_name" placeholder="Ex. Valdez" />
-      </div>
-      <div class="my-4">
-        <Label id="first_name">First Name</Label>
-        <TextInput type="text" id="first_name" placeholder="Ex. Juan" />
-      </div>
-      <div class="my-4">
-        <Label id="middle_name">Middle Name</Label>
-        <TextInput id="middle_name" type="text" placeholder="Ex. Agapito" />
-      </div>
-      <Grid columns="grid grid-cols-3" gap="gap-2">
-        <GridCol>
-          <Label id="suffix">Suffix</Label>
-          <TextInput id="suffix" type="text" placeholder="Ex. Jr" />
-        </GridCol>
-        <GridCol colSpan="col-span-2">
-          <Label id="birthday">Birthday</Label>
-          <DatePicker id="birthday" />
-        </GridCol>
-        <GridCol></GridCol>
-      </Grid>
+      <label class="text-gray-500" for="license_no">License Number</label>
+      <TextInput id="license_no" type="text" />
     </GridCol>
     <GridCol colSpan="col-span-2">
-      <Label id="input-label" classNames="mt-3">Ticket Details</Label>
-      <Divider border={2} />
-      <div class="my-4">
-        <Label id="ticket_number">Ticket Number</Label>
-        <TextInput id="ticket_numbe" type="text" placeholder="Ex. 123123" />
-      </div>
-      <div class="my-4">
-        <Label id="location">Location</Label>
-        <TextInput id="location" type="text" placeholder="Ex. Post 1" />
-      </div>
-      <div class="my-4">
-        <Label id="enforcer">Apprehending Officer</Label>
-        <TextInput id="enforcer" type="text" placeholder="Ex. Agapito" />
-      </div>
-      <Grid columns="grid grid-cols-3" gap="gap-2">
-        <GridCol colSpan="col-span-2">
-          <Label id="violation_date">Apprehension Date</Label>
-          <DatePicker id="violation_date" />
-        </GridCol>
-        <GridCol colSpan="col-span-1">
-          <Label id="violation_time">Apprehension Time</Label>
-          <TextInput type="time" id="violation_time" />
-        </GridCol>
-        <GridCol></GridCol>
-      </Grid>
+      <label class="text-gray-500" for="first_name">First Name</label>
+      <TextInput id="first_name" type="text" />
     </GridCol>
-    <GridCol colSpan="col-span-3">
-      <Label id="input-label" classNames="mt-3">Violations</Label>
-      <Divider border={2} />
+    <GridCol colSpan="col-span-2">
+      <label class="text-gray-500" for="middle_name">Middle Name</label>
+      <TextInput id="middle_name" type="text" />
+    </GridCol>
+    <GridCol colSpan="col-span-2">
+      <label class="text-gray-500" for="last_name">Last Name</label>
+      <TextInput id="last_name" type="text" />
+    </GridCol>
+    <GridCol colSpan="col-span-2">
+      <label class="text-gray-500" for="suffix">Suffix</label>
+      <TextInput id="suffix" type="text" />
+    </GridCol>
+    <GridCol colSpan="col-span-2">
+      <label class="text-gray-500" for="birthdate">Birthdate</label>
+      <TextInput id="birthdate" type="date" />
     </GridCol>
   </Grid>
-  <div class="flex justify-end gap-4 m-11">
-    <Button style="soft" color="gray">Cancel</Button>
-    <Button>Add</Button>
+
+  <Grid columns="grid-cols-2" gap="gap-8" classNames="my-6">
+    <GridCol>
+      <Divider strokeWidth={1}>
+        <h2 slot="left" class="text-gray-600 text-xl">Ticket Information</h2>
+      </Divider>
+      <Grid columns="grid-cols-4" gap="gap-4" classNames="my-4">
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="plate_no">Place Number</label>
+          <TextInput id="plate_no" type="text" />
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="chassis_no">Chassis Number</label>
+          <TextInput id="chassis_no" type="text" />
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="engine_no">Engine Number</label>
+          <TextInput id="engine_no" type="text" />
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="ticket_no">Ticket Number</label>
+          <TextInput id="ticket_no" type="text" />
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="vehicle_type">Vehicle Type</label>
+          <select
+            class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            name="vehicle_type"
+            id="vehicle_type"
+          >
+            <option selected={true} value="0">Open this select menu</option>
+
+            {#if data.vehicleTypes}
+              {#each data.vehicleTypes as type}
+                <option value={type.id}>{type.type}</option>
+              {/each}
+            {/if}
+          </select>
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="location">Location</label>
+          <TextInput id="location" type="text" />
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="violation_date"
+            >Violation Date</label
+          >
+          <TextInput id="violation_date" type="date" />
+        </GridCol>
+        <GridCol colSpan="col-span-2">
+          <label class="text-gray-500" for="violation_time"
+            >Violation Time</label
+          >
+          <TextInput id="violation_time" type="time" />
+        </GridCol>
+      </Grid>
+    </GridCol>
+    <GridCol>
+      <Divider strokeWidth={1}>
+        <h2 slot="left" class="text-gray-600 text-xl">Violations</h2>
+        <Button
+          type="button"
+          slot="right"
+          size="small"
+          style="soft"
+          on:click={() =>
+            open({
+              id: "violations",
+              props: {},
+            })}>Add Violation</Button
+        >
+      </Divider>
+      <div class="overflow-auto h-fit">
+        <div class="flex flex-col gap-2">
+          {#if data.violations}
+            {#each data.violations as violation}
+              <p>{violation.name}</p>
+            {/each}
+          {/if}
+        </div>
+      </div>
+    </GridCol>
+  </Grid>
+  <div class="flex justify-end gap-4 mt-4">
+    <a href="/tickets">
+      <Button type="button" style="soft" color="gray">Cancel</Button>
+    </a>
+    <Button type="submit">Save Ticket</Button>
   </div>
 </form>
+
+<Overlay title="Select Violations" type="canvas" id="violations" let:data
+></Overlay>
