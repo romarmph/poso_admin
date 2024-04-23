b070f2a8c0d4459fedb2e988a2b7ded2243326cd
shit <zedchixx23@gmail.com>
2024-04-23 15:10:05 +0800

    header

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -22,51 +22,9 @@
     <div
       class="w-full flex items-center justify-end ms-auto sm:justify-between sm:gap-x-3 sm:order-3"
     >
-      <div class="sm:hidden">
-        <button
-          type="button"
-          class="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        >
-          <svg
-            class="flex-shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" />
-          </svg>
-        </button>
-      </div>
-
       <div class="hidden sm:block"></div>
 
       <div class="flex flex-row items-center justify-end gap-2">
-        <button
-          type="button"
-          class="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        >
-          <svg
-            class="flex-shrink-0 size-4"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" /><path
-              d="M10.3 21a1.94 1.94 0 0 0 3.4 0"
-            /></svg
-          >
-        </button>
-
         <AvatarDropdown>
           <AvatarDropdownLink>
             <form action="/auth/signout" method="POST">
